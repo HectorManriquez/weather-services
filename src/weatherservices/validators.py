37ee980c349f7746849cb5b49e59faa5496bc2e2
hector <hector.j.manriquez@gmail.com>
2017-10-08 23:48:19 -0500

    Adding new lat lng validator and using it in the getting temperatures view

--- src/weatherservices/validators.py
+++ src/weatherservices/validators.py
@@ -1,6 +1,7 @@
 from rest_framework.validators import ValidationError
 
 from weatherservices.utils import acceptable_filters
+from common.googlemaps import gmaps
 
 
 def filters_validator(filters):
@@ -22,4 +23,16 @@ def number_validator(name, value):
         raise ValidationError({
             'detail': 'Invalid {}'.format(name),
             'description': '{} must be a valid number'.format(name)
+        })
+
+
+def lat_lng_validator(lat, lng):
+    # valid lat 40.714224 lng -73.961452
+    # invalid lat 10 lng -45.53
+    validated = gmaps.reverse_geocode((lat, lng))
+
+    if not validated:
+        raise ValidationError({
+            'detail': 'Invalid filters input.',
+            'description': 'Filters must be list containing one of {}, {}, {}'.format(*acceptable_filters)
         })
--- src/weatherservices/views.py
+++ src/weatherservices/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from weatherservices.utils import get_services_data
-from weatherservices.validators import filters_validator, number_validator
+from weatherservices.validators import filters_validator, number_validator, lat_lng_validator
 
 
 @api_view(['GET', 'POST'])
@@ -17,7 +17,7 @@ def average_temperature(request):
         filters_validator(filters)
         number_validator('lat', lat)
         number_validator('lng', lng)
+        lat_lng_validator(lat, lng)
 
         accu = get_services_data(filters, lat, lng)
-        # print(data)
         return Response(accu, content_type='application/json', status=status.HTTP_200_OK)
