05dc5af6f19808ead255d86dce4530891371c0f8
hector <hector.j.manriquez@gmail.com>
2017-10-09 02:23:24 -0500

    Removing get request from api_view decorator

--- src/weatherservices/views.py
+++ src/weatherservices/views.py
@@ -5,8 +5,7 @@ from rest_framework import status
 from weatherservices.utils import validate_input_get_data
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def average_temperature(request):
     if request.method == 'POST':
-
         return Response(validate_input_get_data(request.data), content_type='application/json', status=status.HTTP_200_OK)
