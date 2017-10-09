158015332117cc2e6b597da32deb434bb49f1e27
hector <hector.j.manriquez@gmail.com>
2017-10-09 02:08:59 -0500

    Moving all view.py logic in here for validating user input and either converting an entered zip code to lat and lng through google maps api or passing the lat and lng through. Lat and lng being entered takes precedence over zip code if both are in the user input. Added zip to lat and lng function for converting zip code through google maps api

--- src/weatherservices/utils.py
+++ src/weatherservices/utils.py
@@ -1,9 +1,18 @@
+from googlemaps.exceptions import HTTPError
+
 from noaa.utils import get_noaa_data
 from weatherdotcom.utils import get_weatherdotcom_data
 from accuweather.utils import get_accuweather_data
+from common.googlemaps import gmaps
+from weatherservices.validators import (
+    acceptable_filters,
+    filters_validator,
+    number_validator,
+    lat_lng_validator,
+    input_validator,
+    zip_code_error
+)
 
-
-acceptable_filters = ['noaa', 'weather.com', 'accuweather']
 services_dict = {
     'noaa': get_noaa_data,
     'weather.com': get_weatherdotcom_data,
@@ -25,6 +34,41 @@ def temps_average(temps_list):
         'fahrenheit': taverage['f'] / len(temps_list),
         'celsius': taverage['c'] / len(temps_list)
     }
+
+
+def validate_input_get_data(user_input):
+    input_validator(user_input)
+
+    lat = None
+    lng = None
+
+    if 'lat' and 'lng' in user_input:
+        lat = user_input['lat']
+        lng = user_input['lng']
+    else:
+        lat_lng = zipcode_to_lat_lng(user_input['zip_code'])
+        lat, lng = lat_lng['lat'], lat_lng['lng']
+
+    filters = user_input['filters']
+
+    filters_validator(filters)
+    number_validator('lat', lat)
+    number_validator('lng', lng)
+    lat_lng_validator(lat, lng)
+
+    return get_services_data(filters, lat, lng)
+
+
+def zipcode_to_lat_lng(zip_code):
+    try:
+        lat_lng = gmaps.geocode(zip_code)
+    except HTTPError:
+        raise zip_code_error
+
+    if not lat_lng:
+        raise zip_code_error
+
+    return lat_lng[0]['geometry']['location']
 
 
 def get_services_data(services_list, lat, lng):
