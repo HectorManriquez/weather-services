933a369e78fd08391ac583183acb2e8c359f3308
hector <hector.j.manriquez@gmail.com>
2017-10-09 02:05:53 -0500

    Moving all logic to outside of the average temperature view

--- src/weatherservices/views.py
+++ src/weatherservices/views.py
@@ -2,22 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from weatherservices.utils import get_services_data
-from weatherservices.validators import filters_validator, number_validator, lat_lng_validator
+from weatherservices.utils import validate_input_get_data
 
 
 @api_view(['GET', 'POST'])
 def average_temperature(request):
     if request.method == 'POST':
-        data = request.data
-        lat = data['lat']
-        lng = data['lng']
-        filters = data['filters']
 
-        filters_validator(filters)
-        number_validator('lat', lat)
-        number_validator('lng', lng)
-        lat_lng_validator(lat, lng)
-
-        accu = get_services_data(filters, lat, lng)
-        return Response(accu, content_type='application/json', status=status.HTTP_200_OK)
+        return Response(validate_input_get_data(request.data), content_type='application/json', status=status.HTTP_200_OK)
